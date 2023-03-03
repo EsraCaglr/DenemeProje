f576d250f8d86b23bda2f615680a55987f49b1ae
Esra <esracaglar9067@gmail.com>
2023-03-03 10:52:49 +0100

    Branch degisiklik

--- src/Test1/test1Deneme.java
+++ src/Test1/test1Deneme.java
@@ -1,4 +1,9 @@
 package Test1;
 
 public class test1Deneme {
+    public static void main(String[] args) {
+        System.out.println("deneme");
+        System.out.println("esra deneme ilk");
+        // dlöashbföäsxdflbh
+    }
 }
